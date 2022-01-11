6e065c83faa6925203f8b880682cd7a9f99a59b5
Axelle HUMBERT <humbertaxelle40@gmail.com>
2022-01-11 23:40:57 +0100

    sound

--- sketch.js
+++ sketch.js
@@ -10,17 +10,11 @@ let value = 150;
 
 function setup() {
   createARCanvas();
-  soundFormats('mp3', 'ogg');
-  mySound = loadSound('BLOOP');
+  // soundFormats('mp3', 'ogg');
+  // mySound = loadSound('BLOOP');
 }
 
 function draw() {
-  rot += rotSpeed;
-  // rotateY(rot);
-
-  // fill(100, 100, 240);
-  // box(5);
-
   if (isPressed%2 == 0) {
     translate(x, y, 0);
     fill(240, 100, 100);
@@ -31,49 +25,11 @@ function draw() {
     fill(100, 100, 240);
     box(5);
   }
-
-  
-  // translate(-5, 0, 0);
-  // fill(240, 100, 100);
-  // box(5);
-
-  // translate(10, 0, 0);
-  // fill(100, 100, 240);
-  // box(5);
-
-  // translate(0, -5, 0);
-  // fill(100, 100, 240);
-  // box(5);
-
-  // translate(0, -5, 0);
-  // fill(100, 100, 240);
-  // box(5);
-
-  // translate(-5, 0, 0);
-  // fill(100, 240, 100);
-  // box(5);
-
-  // translate(0, 5, 0);
-  // fill(100, 240, 100);
-  // box(5);
-
-  // translate(-5, 0, 0);
-  // fill(240, 100, 100);
-  // box(5);
-
-  // translate(0, -5, 0);
-  // fill(240, 100, 100);
-  // box(5);
 }
 
 function touchStarted(){
-  // if (pmouseX <= x+1000 && pmouseX >= x-1000 && pmouseY <= y+1000 && pmouseY <= y-1000){
-  //   isPressed += 1;
-  //   x = Math.random() * max;
-  //   y = Math.random() * max;
-  // }
   isPressed += 1;
   x = Math.random() * max;
   y = Math.random() * max;
-  mySound.play();
+  // mySound.play();
 }
