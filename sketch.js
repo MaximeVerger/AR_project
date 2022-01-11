3669820c6d2406fde209cb2b405fdae2d0a7194e
Axelle HUMBERT <humbertaxelle40@gmail.com>
2022-01-11 20:59:51 +0100

    sound

--- sketch.js
+++ sketch.js
@@ -16,64 +16,23 @@ function setup() {
 
 function draw() {
   rot += rotSpeed;
-  // rotateY(rot);
-
-  // fill(100, 100, 240);
-  // box(5);
 
   if (isPressed%2 == 0) {
     translate(x, y, 0);
     fill(240, 100, 100);
     box(5);
+    mySound.play();
   }
   else {
     translate(x, y, 0);
     fill(100, 100, 240);
     box(5);
+    mySound.play();
   }
-
-  
-  // translate(-5, 0, 0);
-  // fill(240, 100, 100);
-  // box(5);
-
-  // translate(10, 0, 0);
-  // fill(100, 100, 240);
-  // box(5);
-
-  // translate(0, -5, 0);
-  // fill(100, 100, 240);
-  // box(5);
-
-  // translate(0, -5, 0);
-  // fill(100, 100, 240);
-  // box(5);
-
-  // translate(-5, 0, 0);
-  // fill(100, 240, 100);
-  // box(5);
-
-  // translate(0, 5, 0);
-  // fill(100, 240, 100);
-  // box(5);
-
-  // translate(-5, 0, 0);
-  // fill(240, 100, 100);
-  // box(5);
-
-  // translate(0, -5, 0);
-  // fill(240, 100, 100);
-  // box(5);
 }
 
 function touchStarted(){
-  // if (pmouseX <= x+1000 && pmouseX >= x-1000 && pmouseY <= y+1000 && pmouseY <= y-1000){
-  //   isPressed += 1;
-  //   x = Math.random() * max;
-  //   y = Math.random() * max;
-  // }
   isPressed += 1;
   x = Math.random() * max;
   y = Math.random() * max;
-  mySound.play();
 }
