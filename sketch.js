403a5547a44efd26e4a5ef4098e38f799cf4c33f
MaximeVerger <vergermaxime.sio@gmail.com>
2022-01-11 15:39:55 +0100

    test

--- sketch.js
+++ sketch.js
@@ -13,12 +13,6 @@ function draw() {
 
   box(5);
 
-  if (touchIsDown) {
-    value = 255
-  } else {
-    value = 150
-  }
-
   // translate(-5, 0, 0);
   // fill(240, 100, 100);
   // box(5);
@@ -52,12 +46,11 @@ function draw() {
   // box(5);
 }
 
-// function mouseClicked(){
-//   console.log("yes tu click")
-//   if (value === 150){
-//     value = 255
-//   } 
-//   else {
-//     value = 150
-//   }
-// }
+function touchStarted(){
+  if (value === 150){
+    value = 255
+  } 
+  else {
+    value = 150
+  }
+}
