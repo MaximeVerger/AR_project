8baba363a4a74f094b5725fcda167f490b3bb54b
Axelle HUMBERT <humbertaxelle40@gmail.com>
2022-01-11 21:41:39 +0100

    sound

--- sketch.js
+++ sketch.js
@@ -6,41 +6,73 @@ let max = 15;
 let x = 0;
 let y = 0;
 let value = 150;
-let loaddone = False
 
-
-function preload() {
-  // preload() runs once
-  console.log("preload");
-  loaddone = True
-}
 
 function setup() {
-  let canvas = createARCanvas();
-  alert("canvas done !")
-  preload();
-  if(loaddone == True){ alert("preload done !")  }
+  createARCanvas();
+  new p5.SoundFile("BLOOP.mp3", touchStarted);
 }
 
 function draw() {
   rot += rotSpeed;
-  
+  // rotateY(rot);
+
+  // fill(100, 100, 240);
+  // box(5);
+
   if (isPressed%2 == 0) {
     translate(x, y, 0);
     fill(240, 100, 100);
     box(5);
-    mySound.play();
   }
   else {
     translate(x, y, 0);
     fill(100, 100, 240);
     box(5);
-    mySound.play();
   }
+
+  
+  // translate(-5, 0, 0);
+  // fill(240, 100, 100);
+  // box(5);
+
+  // translate(10, 0, 0);
+  // fill(100, 100, 240);
+  // box(5);
+
+  // translate(0, -5, 0);
+  // fill(100, 100, 240);
+  // box(5);
+
+  // translate(0, -5, 0);
+  // fill(100, 100, 240);
+  // box(5);
+
+  // translate(-5, 0, 0);
+  // fill(100, 240, 100);
+  // box(5);
+
+  // translate(0, 5, 0);
+  // fill(100, 240, 100);
+  // box(5);
+
+  // translate(-5, 0, 0);
+  // fill(240, 100, 100);
+  // box(5);
+
+  // translate(0, -5, 0);
+  // fill(240, 100, 100);
+  // box(5);
 }
 
 function touchStarted(){
+  // if (pmouseX <= x+1000 && pmouseX >= x-1000 && pmouseY <= y+1000 && pmouseY <= y-1000){
+  //   isPressed += 1;
+  //   x = Math.random() * max;
+  //   y = Math.random() * max;
+  // }
   isPressed += 1;
   x = Math.random() * max;
   y = Math.random() * max;
+  mySound.play();
 }
