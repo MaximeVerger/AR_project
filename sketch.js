f06eaa6797798cf863881874065bb70d69206ed1
MaximeVerger <vergermaxime.sio@gmail.com>
2022-01-12 11:42:39 +0100

    test

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ let mySound;
 let rot = 0;
 let rotSpeed = 0.1;
 let isPressed = 0;
-let max = 15;
+let max = 25;
 let x = 0;
 let y = 0;
+let z = 0;
+let rand = 0;
 let value = 150;
 
 
@@ -16,12 +18,12 @@ function setup() {
 
 function draw() {
   if (isPressed%2 == 0) {
-    translate(x, y, 0);
+    translate(x, y, z);
     fill(240, 100, 100);
     box(5);
   }
   else {
-    translate(x, y, 0);
+    translate(x, y, z);
     fill(100, 100, 240);
     box(5);
   }
@@ -29,7 +31,8 @@ function draw() {
 
 function touchStarted(){
   isPressed += 1;
-  x = Math.random() * max;
-  y = Math.random() * max;
+  x = Math.random() * max + 5;
+  y = Math.random() * max + 5;
+  z = Math.random() * max + 5;
   // mySound.play();
 }
